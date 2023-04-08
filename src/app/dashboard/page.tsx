2f5e5ac5ecc19e51a6f6027c52b6e63dc616ba89
krishnasannidhi <krishna.sannidhi@gmail.com>
2023-04-08 08:35:03 -0500

    initial commit with typescript and classvariance

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react";
+
+interface pageProps {}
+
+const page: FunctionComponent<pageProps> = () => {
+  return <div>Dashboard Page</div>;
+};
+
+export default page;
