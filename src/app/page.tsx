618c15ef9c1c2a09fb7adafcbced60ac7b38e179
krishnasannidhi <krishna.sannidhi@gmail.com>
2023-04-08 10:05:21 -0500

    upstash redis setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 import Button from "@/components/ui/Button";
+import { db } from "@/lib/db";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default async function Home() {
+  await db.set("hello", "hello");
   return <Button size="deafult">Hello</Button>;
 }
