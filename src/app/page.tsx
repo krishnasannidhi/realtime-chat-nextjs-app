76d5c2807515f5a2b6fdd819c45b52206e2561dc
krishnasannidhi <krishna.sannidhi@gmail.com>
2023-04-08 09:26:01 -0500

    added new class packages and updated button comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
+import Button from "@/components/ui/Button";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div className="text-red-500">Hello World</div>;
+  return <Button size="deafult">Hello</Button>;
 }
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,7 @@
-import { cva } from "class-variance-authority";
-import { FunctionComponent } from "react";
-
-interface ButtonProps {}
+import { cn } from "@/lib/utils";
+import { VariantProps, cva } from "class-variance-authority";
+import { Loader2 } from "lucide-react";
+import { ButtonHTMLAttributes, FunctionComponent } from "react";
 
 const buttonVariants = cva(
   "active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none",
@@ -23,8 +23,31 @@ const buttonVariants = cva(
     },
   }
 );
-const Button: FunctionComponent<ButtonProps> = () => {
-  return <div>Button</div>;
+
+export interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  isLoading?: boolean;
+}
+
+const Button: FunctionComponent<ButtonProps> = ({
+  className,
+  children,
+  variant,
+  isLoading,
+  size,
+  ...props
+}) => {
+  return (
+    <button
+      className={cn(buttonVariants({ variant, size, className }))}
+      disabled={isLoading}
+      {...props}
+    >
+      {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+      {children}
+    </button>
+  );
 };
 
 export default Button;
